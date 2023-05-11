021968471d6f37d2018ed81f4fe2c6438b22a05a
Emma Gerigscott <elgerig@gmail.com>
2023-05-11 12:01:57 -0700

    add cute drawings, header, footer, add static content loader
    Co-authored-by: Sarah Reimann <reimannsarah45@gmail.com>

--- Tamagotchi/Program.cs
+++ Tamagotchi/Program.cs
@@ -14,7 +14,7 @@ namespace Tamagotchi
       WebApplication app = builder.Build();
 
       app.UseHttpsRedirection();
-
+      app.UseStaticFiles();
       app.UseRouting();
 
       app.MapControllerRoute(
